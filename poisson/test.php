87a497e0c7df35f21f8de8869a3e124c82ed186c
leitzzz <leitzner.aguey@gmail.com>
2020-06-27 21:06:52 -0500

    fix main class route for poisson calculation

--- poisson/test.php
+++ poisson/test.php
@@ -1,9 +1,11 @@
 <?php
-
-include("Poisson.php");
+// simple poisson calculation.
+// extra info in spanish: https://www.inteapuestas.com/marcador-exacto.htm
+include("classes/Poisson.php");
 
 $poisson = new Poisson;
 
+// for example if is a average of occurrence of 1.8 and we want to test the probability of occurs 1.
 echo $poisson->calculate(1.8, 1);
 
 ?>
